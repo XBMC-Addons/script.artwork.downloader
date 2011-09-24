82ea95a02de072e40632e29adb1eb7167227242e
paddycarey <paddy@wackwack.co.uk>
2011-09-24 13:30:42 +0100

    Don't add TVShow to TVlist if cannot get imdbnumber

--- script.extrafanartdownloader/default.py
+++ script.extrafanartdownloader/default.py
@@ -14,7 +14,7 @@ class EFDL:
 
         self.fanart_count = 0
         xbmc.log('EFDL: Extrafanart Downloader initialising')
-        xbmc.executebuiltin("XBMC.Notification('Extrafanart Downloader','Starting',5000)")
+        xbmc.executebuiltin("XBMC.Notification(Extrafanart Downloader,Starting,5000)")
 
         if (xbmc.getCondVisibility('Library.IsScanningVideo')  == False):
             self.TV_listing()
@@ -44,11 +44,11 @@ class EFDL:
                                 xbmc.log('EFDL: Downloaded fanart for %s: %s' % (self.show_name, fanarturl))
                                 self.fanart_count = self.fanart_count + 1
                     else:
-                        xbmc.log('EFDL: Processed \'%s\'' % self.show_name)
+#                        xbmc.log('EFDL: Processed \'%s\'' % self.show_name)
                         break
         else:
             xbmc.log('EFDL: Library Update is running: Aborting')
-            xbmc.executebuiltin("XBMC.Notification('Extrafanart Downloader','Library Update is running: Aborting',5000)")
+            xbmc.executebuiltin("XBMC.Notification(Extrafanart Downloader,Library Update is running: Aborting,5000)")
 
     def TV_listing(self):
         json_query = xbmc.executeJSONRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetTVShows", "params": {"properties": ["file", "imdbnumber"], "sort": { "method": "label" } }, "id": 1}')
@@ -64,13 +64,11 @@ class EFDL:
                     findimdbnumber = re.search( '"imdbnumber":"(.*?)","', tvshowitem )
                     if findimdbnumber:
                         imdbnumber = (findimdbnumber.group(1))
-                    else:
-                        imdbnumber = ''
-                    TVshow = {}
-                    TVshow["name"] = tvshowname
-                    TVshow["id"] = imdbnumber
-                    TVshow["path"] = path
-                    self.TVlist.append(TVshow)
+                        TVshow = {}
+                        TVshow["name"] = tvshowname
+                        TVshow["id"] = imdbnumber
+                        TVshow["path"] = path
+                        self.TVlist.append(TVshow)
 
 
 xbmc.executebuiltin('CancelAlarm(extrafanart)')
@@ -87,8 +85,8 @@ EFDLrun = EFDL()
 xbmc.log('EFDL: Extrafanart Downloader finished: Script will run again in %s minutes' % timer_amounts[Addon.getSetting('timer_amount')])
 
 if EFDLrun.fanart_count > 0:
-    xbmc.executebuiltin("XBMC.Notification('Extrafanart Downloader','Finished: %s extrafanart downloaded',5000)" % EFDLrun.fanart_count)
+    xbmc.executebuiltin("XBMC.Notification(Extrafanart Downloader,Finished: %s extrafanart downloaded,5000)" % EFDLrun.fanart_count)
 
-xbmc.executebuiltin("XBMC.Notification('Extrafanart Downloader','Finished: Next run in %s minutes',3000)" % timer_amounts[Addon.getSetting('timer_amount')])
+xbmc.executebuiltin("XBMC.Notification(Extrafanart Downloader,Finished: Next run in %s minutes,3000)" % timer_amounts[Addon.getSetting('timer_amount')])
 
 xbmc.executebuiltin('AlarmClock(extrafanart,XBMC.RunScript(script.extrafanartdownloader),' + timer_amounts[Addon.getSetting('timer_amount')] +  ',true)')
