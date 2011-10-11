39a52cabf1cede076d131deebc0abbd5851cb8d2
paddycarey <paddy@wackwack.co.uk>
2011-10-12 00:05:44 +0100

    Only download specified amount of fanart

--- default.py
+++ default.py
@@ -212,6 +212,8 @@ class Main:
                                 break
                             fanartfile = provider.get_filename(fanarturl)
                             self.current_fanart = self.current_fanart + 1
+                            if self.limit_extrafanart_max and self.current_fanart > self.limit_extrafanart_max:
+                                break
                             try:
                                 self.fileops._downloadfile(fanarturl, fanartfile, targetdirs)
                             except HTTP404Error as e:
