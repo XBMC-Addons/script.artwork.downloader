078201538b84158f0b74394a180f70788eace5e5
Machine-Sanctum <machine-sanctum@zeelandnet.nl>
2011-10-21 17:52:51 +0200

    Added Dharma10.1 SMB:// share support. Thanks to Giftie for getting me on the right track.

--- resources/lib/fileops.py
+++ resources/lib/fileops.py
@@ -11,6 +11,7 @@ if xbmc_version == 'Eden':
     import xbmcvfs
 else:
     import shutil
+    from resources.lib.smbclient import smbclient
 
 log = utils._log
 
@@ -38,7 +39,7 @@ class fileops:
             self._delete = lambda path: xbmcvfs.delete(path)
 
         self.downloadcount = 0
-        addondir = xbmc.translatePath('special://profile/addon_data/%s' % utils.__addonid__)
+        addondir = xbmc.translatePath( utils.__addon__.getAddonInfo('profile') )
         self.tempdir = os.path.join(addondir, 'temp')
         if not self._exists(self.tempdir):
             if not self._exists(addondir):
@@ -66,16 +67,16 @@ class fileops:
                     return False
             else:
                 return True
-        def _mkdir(self, path):
-            try:
-                os.makedirs(path)
-            except:
-                if os.path.exists(path):
-                    return True
-                else:
-                    return False
-            else:
-                return True
+#        def _mkdir(self, path):
+#            try:
+#               os.mkdir(path)
+#            except:
+#                if os.path.exists(path):
+#                    return True
+#                else:
+#                    return False
+#            else:
+#                return True
         def _delete(self, path):
             try:
                 os.remove(path)
@@ -90,8 +91,84 @@ class fileops:
                 return False
             else:
                 return True
-            
-            
+        def _mkdir( self, path ):
+            try:
+               os.mkdir(path)
+            except:
+                if os.path.exists(path):
+                    return True
+                else:
+                    log( "Building Directory", xbmc.LOGDEBUG )
+                    if path.startswith( "smb://" ) and not os.environ.get( "OS", "win32" ) in ("win32", "Windows_NT"):
+                        self._smb_mkdir( path )
+                        return True
+                    if ( path.startswith( "smb://" ) and os.environ.get( "OS", "win32" ) in ("win32", "Windows_NT") ):
+                        log( "Building Samba Share Directory on Windows System", xbmc.LOGDEBUG )
+                        if "@" in path:
+                            path = "\\\\" + path.split("@")[1]
+                        path = path.replace( "/", "\\" ).replace( "smb:", "" )
+                    # no need to create folders
+                    if ( os.path.isdir( path ) ): return True
+                    # temp path
+                    tmppath = path
+                    # loop thru and create each folder
+                    while ( not os.path.isdir( tmppath ) ):
+                        try:
+                            os.mkdir( tmppath )
+                        except:
+                            tmppath = os.path.dirname( tmppath )
+                    # call function until path exists
+                    self._mkdir( path )
+                    if os.path.exists(path):
+                        log('Succesfully created folder on Samba Share Directory')
+                        return True
+            else:
+                return True 
+
+        def _smb_mkdir( self, path ):
+            log( "Building Samba Directory on Non Windows System", xbmc.LOGDEBUG )
+            if self._exists( path ):
+                return
+            # setup for samba communication
+            samba_list = path.split( "/" )
+            print samba_list
+            remote_share = samba_list[ 3 ]
+            if "@" in samba_list[ 2 ]:
+                remote_name = samba_list[ 2 ].split( "@" )[1]
+                samba_user = ( samba_list[ 2 ].split( "@" )[0] ).split( ":" )[0]
+                samba_pass = ( samba_list[ 2 ].split( "@" )[0] ).split( ":" )[1]
+            else:
+                remote_name = samba_list[ 2 ]
+                try:
+                    if utils.__addon__.getSetting( "protectedshare" ) == "true":
+                        samba_user = utils.__addon__.getSetting( "samba_user" )
+                        samba_pass = utils.__addon__.getSetting( "samba_pass" )
+                    else:
+                        # default to guest if no user/pass is given
+                        samba_user = None
+                        samba_pass = None
+                except:
+                    samba_user = None
+                    samba_pass = None
+            log( "Samba - Remote Name: %s" % remote_name, xbmc.LOGDEBUG )
+            log( "Samba - Remote Share: %s" % remote_share, xbmc.LOGDEBUG )
+            log( "Samba - Username: %s" % samba_user, xbmc.LOGDEBUG )
+            log( "Samba - Password: %s" % samba_pass, xbmc.LOGDEBUG )
+            smb = smbclient.SambaClient( server=remote_name, share=remote_share,
+                                        username=samba_user, password=samba_pass )
+            path2 = "smb://" + remote_name + "/" + "/".join( samba_list[3:] )
+            tmppath = "/".join( samba_list[4:] )
+            while( not ( self._exists( path2 ) or path2 == "smb:" ) ):
+                print path2
+                try:
+                    log( "Attempting making direcory: %s" % tmppath, xbmc.LOGDEBUG )
+                    smb.mkdir( get_unicode( tmppath ) )
+                except:
+                    tmppath = os.path.dirname( tmppath )
+                    # need to strip the same part from a true path for the exists option
+                    path2 = os.path.dirname( path2 )
+            self._smb_mkdir( path )
+
     def _delete_file_in_dirs(self, filename, targetdirs, reason):
         """
         Delete file from all targetdirs
