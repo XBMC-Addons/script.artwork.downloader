f90e81de74df42df6c410d78208598cd3495fe2e
MartijnKaijser <machine-sanctum@zeelandnet.nl>
2011-12-07 23:53:06 +0100

    prepare for movie API

--- resources/lib/provider/__init__.py
+++ resources/lib/provider/__init__.py
@@ -1,6 +1,7 @@
 from tvdb import TVDBProvider
 from tmdb import TMDBProvider
 from fanarttv import FTV_TVProvider
+#from fanarttv import FTV_MovieProvider
 
 def get_providers():
     movie_providers = []
