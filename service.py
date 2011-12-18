c2c3b9921e4183c05e8c62ba9de59615efbda43f
Martijn Kaijser <machine-sanctum@zeelandnet.nl>
2011-12-18 21:42:53 +0100

    Fix warning log message when temp folder didn't exist

--- service.py
+++ service.py
@@ -21,7 +21,11 @@ def autostart():
         log('Service - Run at startup: %s'%settings.service_startup, xbmc.LOGNOTICE)        
         log('Service - Run as service: %s'%settings.service_enable, xbmc.LOGNOTICE)
         log('Service - Time: %s:00'%service_runtime, xbmc.LOGNOTICE)
-        xbmcvfs.rmdir(tempdir)
+        try:
+            xbmcvfs.rmdir(tempdir)
+            log('Removing temp folder from previous runs')
+        except:
+            log('No temp folder found from previous run. Continue starting service')
         if settings.service_startup:
             time.sleep(10)
             xbmc.executebuiltin('XBMC.RunScript(script.artwork.downloader,silent=true)')
