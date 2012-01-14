c0130f02065fc8718b5d080c4376195ebdf550d1
MartijnKaijser <machine.sanctum@gmail.com>
2012-01-14 02:54:48 +0100

    added some languages

--- resources/lib/language.py
+++ resources/lib/language.py
@@ -4,14 +4,20 @@ import xbmcaddon
 def get_abbrev():
     language = xbmcaddon.Addon().getSetting("limit_language").upper()
     translates = {
-        'DUTCH'     : 'nl',
-        'ENGLISH'   : 'en',
-        'FRENCH'    : 'fr',
-        'GERMAN'    : 'de',
-        'ITALIAN'   : 'it',
-        'POLISH'    : 'pl',
-        'RUSSIAN'   : 'ru',
-        'SPANISH'   : 'es'
+        'CHINESE'       : 'zh',
+        'DUTCH'         : 'nl',
+        'ENGLISH'       : 'en',
+        'FINNISH'       : 'fi',
+        'FRENCH'        : 'fr',
+        'GERMAN'        : 'de',
+        'HUNGARIAN'     : 'hu',
+        'ITALIAN'       : 'it',
+        'JAPANESE'      : 'ja',
+        'POLISH'        : 'pl',
+        'PORTUGUESE'    : 'pt',
+        'RUSSIAN'       : 'ru',
+        'SPANISH'       : 'es',
+        'SWEDISH'       : 'sv'
     }
     if language in translates:
         return translates[language]
