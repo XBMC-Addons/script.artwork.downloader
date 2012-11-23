ffbeb018956b372adb6709ba69f349aaf1ccbc2a
Martijn Kaijser <mcm.kaijser@gmail.com>
2012-11-23 21:53:02 +0100

    remove API sleep that isn't needed or works anymore

--- resources/lib/utils.py
+++ resources/lib/utils.py
@@ -137,8 +137,6 @@ def get_data_new(url, data_type):
         if url.startswith("http://api.themoviedb.org"):
             request.add_header("Accept", "application/json")
         # Add some delay to stop trashing the fanart.tv server
-        elif url.startswith("http://fanart.tv/"):
-            xbmc.sleep(1000)
         req = urllib2.urlopen(request)
         if data_type == 'json':
             data = simplejson.loads(req.read())
