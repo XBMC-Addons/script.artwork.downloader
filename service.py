70353389e50620c785e97a060e1c7bdde7b26133
Martijn Kaijser <mcm.kaijser@gmail.com>
2013-05-11 11:37:43 +0200

    also use new settings lib in the service

--- service.py
+++ service.py
@@ -18,20 +18,19 @@ __localize__    = __addon__.getLocalizedString
 __addonprofile__= xbmc.translatePath( __addon__.getAddonInfo('profile') ).decode('utf-8')
 
 #import libraries
-from resources.lib.utils import *
-from resources.lib.settings import settings
+from resources.lib.utils import log
+from lib.settings import get
+setting = get()
 
 # starts update/sync
 def autostart():
         xbmcaddon.Addon().setSetting(id="files_overwrite", value='false')
-        setting = settings()
-        setting._get_general()
         tempdir = os.path.join(__addonprofile__, 'temp')
-        service_runtime  = str(setting.service_runtime + ':00')
-        log('## Service - Run at startup: %s'%setting.service_startup, xbmc.LOGNOTICE)        
-        log('## Service - Delayed startup: %s minutes'%setting.service_startupdelay, xbmc.LOGNOTICE)   
-        log('## Service - Run as service: %s'%setting.service_enable, xbmc.LOGNOTICE)
-        log('## Service - Time: %s'%service_runtime, xbmc.LOGNOTICE)
+        service_runtime  = str(setting.get('service_runtime') + ':00')
+        log('## Service - Run at startup: %s'% setting.get('service_startup'), xbmc.LOGNOTICE)        
+        log('## Service - Delayed startup: %s minutes'% setting.get('service_startupdelay'), xbmc.LOGNOTICE)   
+        log('## Service - Run as service: %s'% setting.get('service_enable'), xbmc.LOGNOTICE)
+        log('## Service - Time: %s'% service_runtime, xbmc.LOGNOTICE)
         log("##########........................")
         # Check if tempdir exists and remove it
         if xbmcvfs.exists(tempdir):
@@ -41,9 +40,9 @@ def autostart():
         # Run script when enabled and check on existence of tempdir.
         # This because it is possible that script was running even when we previously deleted it.
         # Could happen when switching profiles and service gets triggered again
-        if setting.service_startup and not xbmcvfs.exists(tempdir):
-            xbmc.executebuiltin('XBMC.AlarmClock(ArtworkDownloader,XBMC.RunScript(script.artwork.downloader,silent=true),00:%s:15,silent)' %setting.service_startupdelay) 
-        if setting.service_enable:
+        if setting.get('service_startup') and not xbmcvfs.exists(tempdir):
+            xbmc.executebuiltin('XBMC.AlarmClock(ArtworkDownloader,XBMC.RunScript(script.artwork.downloader,silent=true),00:%s:15,silent)' % setting.get('setting.service_startupdelay')) 
+        if setting.get('service_enable'):
             while (not xbmc.abortRequested):
                 xbmc.sleep(5000)
                 if not(time.strftime('%H:%M') == service_runtime):
